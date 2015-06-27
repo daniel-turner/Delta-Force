c7a04a1f85063d90a8cf2643c03200a171981efb
danturne <danturne@yahoo.com>
2015-06-27 11:01:55 -1000

    completed control

--- test/delta-force-spec.js
+++ test/delta-force-spec.js
@@ -25,5 +25,74 @@ describe("Timer", function() {
     testTimer.should.be.an.instanceof(EventEmitter);
   })
 
+  var testtime = null;
+  var testcount = null;
 
+  it('timer tick event should return time and count', function(done) {
+
+    testTimer = new timers.timer(0);
+    testTimer.addListener('tick', function(tickEvent) {
+
+      testtime = tickEvent.tickTime;
+      testcount = tickEvent.tickCount;
+    });
+
+    setTimeout(function(){
+
+      testtime.should.not.be.null;
+      testcount.should.not.be.null;
+      done();
+    }, 1000);
+  });
+
+
+});
+
+describe('Control', function() {
+
+  it('control is a function', function() {
+
+    timers.control.should.be.a('function');
+  });
+
+  var testControl;
+  var testtime = null;
+
+  it('control.start should emit a start event', function(done) {
+
+    testControl = new timers.control(1000);
+    testControl.addListener('start', function(startEvent) {
+
+      testtime = startEvent.startTime;
+    });
+
+    testControl.start();
+
+    setTimeout(function() {
+
+      testtime.should.not.be.null;
+      done();
+    }, 1500);
+  });
+
+  it('control.stop should emit a stop event', function(done) {
+
+    testtime = null;
+
+    testControl = new timers.control(1000);
+    testControl.addListener('stop', function(stopEvent) {
+
+      testtime = stopEvent.stopTime;
+    });
+
+    testControl.start();
+    testControl.stop();
+
+    setTimeout(function() {
+
+      testtime.should.not.be.null;
+      done();
+    }, 1000);
+
+  });
 })
