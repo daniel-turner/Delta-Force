8884e4caeafd4a2ba660950ad5c5f078425f176a
danturne <danturne@yahoo.com>
2015-06-27 14:29:37 -1000

    timelimit done?

--- test/delta-force-spec.js
+++ test/delta-force-spec.js
@@ -101,4 +101,40 @@ describe('Control', function() {
     }, 1000);
 
   });
-})
+});
+
+describe('TimeLimit', function() {
+  it('timelimit is a function', function() {
+
+    timers.timelimit.should.be.a('function');
+  });
+
+  it('timelimit accepts numbers as input', function() {
+
+    expect(timers.timelimit.bind(timers.timelimit, {},5)).to.throw('TimeLimit was set with an invalid maxTime.');
+    expect(timers.timelimit.bind(timers.timelimit, NaN,5)).to.throw('TimeLimit cannot be set with a NaN maxTime.');
+    expect(timers.timelimit.bind(timers.timelimit, 10, NaN)).to.throw('Control cannot be set with a NaN interval.');
+  });
+
+  var testTimeLimit;
+
+  it('timelimit should emit a complete event after the designated number of ticks', function(done) {
+
+    testtime = null;
+    testTimeLimit = new timers.timelimit(10,5);
+    testTimeLimit.addListener('complete', function(completeEvent) {
+
+      testtime = completeEvent.totalTime;
+    });
+
+    testTimeLimit.start();
+
+    setTimeout(function() {
+
+      testtime.should.not.be.null;
+      testtime.should.be.above(50);
+      testtime.should.be.below(70);
+      done();
+    }, 60);
+  });
+});
